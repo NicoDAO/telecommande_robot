9271f951765dd8b6a9e0abce5e28ed5a6f2af3d9
Nicolas <nicolas2020@nicda.fr>
2020-11-08 15:05:32 +0100

    connection ssh avec bouton et la coroutine

--- app/src/main/java/com/telecommande_robot/MainActivity.kt
+++ app/src/main/java/com/telecommande_robot/MainActivity.kt
@@ -2,17 +2,24 @@ package com.telecommande_robot
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jcraft.jsch.Channel
 import com.jcraft.jsch.ChannelExec
 import com.jcraft.jsch.JSch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.InputStream
 import java.io.OutputStream
 import java.lang.Thread.sleep
 import java.util.*
+
+
 
 sealed class Result<out R> {
     data class Success<out T>(val data: T) : Result<T>()
@@ -24,7 +31,11 @@ class MainActivity : AppCompatActivity() {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
+
     external fun stringFromJNI(): String
+    val connectSSH = LoginRepository()
+    val log_vac_coroutine = LoginViewModel(connectSSH)
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -39,18 +50,36 @@ class MainActivity : AppCompatActivity() {
         gauche.setOnClickListener {
             Toast.makeText(this@MainActivity, "gauche.", Toast.LENGTH_SHORT).show()
             println("gauche.setOnClickListener")
+            // connectSSH.makeLoginRequest()
+            lance = 1;// connectSSH.makeLoginRequest()
 
-           lance =1;// connectSSH.makeLoginRequest()
+        }
+
+
+        val droite = findViewById(R.id.droite) as Button
+        droite.setOnClickListener {
+            Toast.makeText(this@MainActivity, "droite.", Toast.LENGTH_SHORT).show()
+            println("droite.setOnClickListener")
+            connectSSH.makeLoginRequest()
+            //lance =1;// connectSSH.makeLoginRequest()
+
+        }
+        val avance = findViewById(R.id.avance) as Button
+        avance.setOnClickListener {
+            Toast.makeText(this@MainActivity, "avance.", Toast.LENGTH_SHORT).show()
+            println("avance.setOnClickListener")
+            log_vac_coroutine.login("coucou","moi")
+           // connectSSH.makeLoginRequest()
+            //lance =1;// connectSSH.makeLoginRequest()
 
         }
 
         // Example of a call to a native method
         findViewById<TextView>(R.id.puissancemoteur).text = stringFromJNI()
         val texte = findViewById(R.id.connectSSH) as TextView
-        val   connectSSH =   LoginRepository()
 
         Thread(Runnable {
-            while(true) {
+            while (true) {
                 if (lance == 1) {
                     lance = 0
                     connectSSH.makeLoginRequest()
@@ -58,7 +87,51 @@ class MainActivity : AppCompatActivity() {
                 sleep(1000)
             }
             // performing some dummy time taking operation
-          /*
+
+            // try to touch View of UI thread
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                // texte.setText("cocc")
+            })
+        }).start()
+        val thread = SimpleThread()
+        //  thread.start()
+
+
+        //  setupWorkManagerJob()
+
+    }
+
+    override fun onActivityReenter(resultCode: Int, data: Intent?) {
+        super.onActivityReenter(resultCode, data)
+    }
+
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+
+
+        /**
+         * Setup WorkManager background job to 'fetch' new network data daily.
+         */
+    }
+
+    class SimpleThread : Thread() {
+
+        public override fun run() {
+            while (true) {
+                println("${Thread.currentThread()} has run.")
+
+                sleep(1000);
+            }
+        }
+    }
+
+
+    class LoginRepository() {
+
+        fun makeLoginRequest(): Result.Success<String> {
             var i = 0;
 
             val config = Properties()
@@ -78,15 +151,15 @@ class MainActivity : AppCompatActivity() {
             (channel as ChannelExec).setCommand("ls")
             channel.connect()
             while (channel.isConnected()) {
-                println("connect" )
-               // val lit = `in`.read()
+                println("connect")
+                // val lit = `in`.read()
                 //println("   :" + lit)
                 Thread.sleep(100);
             }
-             while (`in`.available() > 0) {
+            while (`in`.available() > 0) {
 
                 val lit = `in`.read().toByte()
-                val formatted = String.format("%c", lit) ;
+                val formatted = String.format("%c", lit);
                 println("on lit " + formatted)
                 sleep(10)
             }
@@ -99,96 +172,26 @@ class MainActivity : AppCompatActivity() {
             }
             println("has runttourne")
             channel.disconnect();
-            session.disconnect();*/
-            // try to touch View of UI thread
-            this@MainActivity.runOnUiThread(java.lang.Runnable {
-                // texte.setText("cocc")
-            })
-        }).start()
-        val thread = SimpleThread()
-        //  thread.start()
-
-
-      //  setupWorkManagerJob()
-
-    }
-    override fun onActivityReenter(resultCode: Int, data: Intent?) {
-        super.onActivityReenter(resultCode, data)
-    }
-
-    companion object {
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
+            session.disconnect();
+            return Result.Success("okok")
         }
 
 
+    }
 
 
-        /**
-         * Setup WorkManager background job to 'fetch' new network data daily.
-         */
-          }
+    class LoginViewModel(
+            private val loginRepository: MainActivity.LoginRepository
+    ) : ViewModel() {
 
-    class SimpleThread : Thread() {
+        fun login(username: String, token: String) {
+            // Create a new coroutine to move the execution off the UI thread
+            viewModelScope.launch(Dispatchers.IO) {
+                val jsonBody = "{ username: \"$username\", token: \"$token\"}"
+                loginRepository.makeLoginRequest()
 
-        public override fun run() {
-            while (true) {
-                println("${Thread.currentThread()} has run.")
-
-                sleep(1000);
             }
         }
     }
 
-
-
-
-class LoginRepository() {
-
-    fun makeLoginRequest(): Result.Success<String> {
-        var i = 0;
-
-        val config = Properties()
-        config.put("StrictHostKeyChecking", "no")
-        config.put("PreferredAuthentications", "publickey,keyboard-interactive,password");
-
-        val jsch = JSch()
-        val session = jsch.getSession("android", "10.0.0.13", 22)
-        session.setPassword("android")
-        session.setConfig(config)
-        session.connect(3000)//connection avec un timeout de 3 secondes
-        println("Connected")
-        val channel: Channel = session.openChannel("exec")
-        var `in`: InputStream = channel.getInputStream()
-        val out: OutputStream = channel.getOutputStream()
-        (channel as ChannelExec).setErrStream(System.err)
-        (channel as ChannelExec).setCommand("ls")
-        channel.connect()
-        while (channel.isConnected()) {
-            println("connect")
-            // val lit = `in`.read()
-            //println("   :" + lit)
-            Thread.sleep(100);
-        }
-        while (`in`.available() > 0) {
-
-            val lit = `in`.read().toByte()
-            val formatted = String.format("%c", lit);
-            println("on lit " + formatted)
-            sleep(10)
-        }
-        if (channel.isClosed()) {
-            println("exit-status: " + channel.getExitStatus())
-        }
-        try {
-            sleep(1000)
-        } catch (ee: Exception) {
-        }
-        println("has runttourne")
-        channel.disconnect();
-        session.disconnect();
-        return Result.Success("okok")
-     }
-}
 }
